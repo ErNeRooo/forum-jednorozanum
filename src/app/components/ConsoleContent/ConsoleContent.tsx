69e8faefbae61e947ce7e316b9c2187d4f99272f
ErNeRooo <erneroooooo@gmail.com>
2024-09-07 18:06:54 +0200

    add tabulation

--- src/app/components/ConsoleContent/ConsoleContent.tsx
+++ src/app/components/ConsoleContent/ConsoleContent.tsx
@@ -13,7 +13,13 @@ const ConsoleContent = () => {
   return (
     <div className={styles.ConsoleContent}>
       {consoleHistory.map((item, index) => {
-        return <span key={index}>{item}</span>;
+        if (item[0] === "P" && item[1] === "S")
+          return <span key={index}>{item}</span>;
+        return (
+          <span className={styles.response} key={index}>
+            {item}
+          </span>
+        );
       })}
       <ConsolePrompt handler={handleOnEnter} />
     </div>
