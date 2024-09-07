dda12ce98547634f39c32a56abef1fdf3d7aeee2
ErNeRooo <erneroooooo@gmail.com>
2024-09-07 18:03:14 +0200

    add ConsoleHeader component

--- src/app/HomePage/HomePage.tsx
+++ src/app/HomePage/HomePage.tsx
@@ -1,23 +1,11 @@
 import styles from "./HomePage.module.sass";
 import ConsoleContent from "../components/ConsoleContent/ConsoleContent";
-
+import ConsoleHeader from "../components/ConsoleHeader/ConsoleHeader";
 const HomePage = () => {
-  const consoleHistory: string[] = [];
-
   return (
     <div className={styles.HomePage}>
-      <span>{"HexOS PowerShell"}</span>
-      <span>
-        {
-          "Copyright (C) Hexagon Industries and Scientific Research Agency. All rights reserved."
-        }
-      </span>
-      <br />
-      <span>{"PS C:\\forum-jednorozanum> help"}</span>
-      <span>{"AVAILABLE COMMANDS:"}</span>
-      <span>{" - forum login (to log into your account)"}</span>
-      <span>{" - forum register (to create new account)"}</span>
-      <ConsoleContent></ConsoleContent>
+      <ConsoleHeader />
+      <ConsoleContent />
     </div>
   );
 };
---
+++ src/app/components/ConsoleHeader/ConsoleHeader.tsx
@@ -0,0 +1,25 @@
+import styles from "./ConsoleHeader.module.sass";
+
+const ConsoleHeader = () => {
+  return (
+    <div className={styles.ConsoleHeader}>
+      <span>{"HexOS PowerShell"}</span>
+      <span>
+        {
+          "Copyright (C) Hexagon Industries and Scientific Research Agency. All rights reserved."
+        }
+      </span>
+      <br />
+      <span>{"PS C:\\forum-jednorozanum> help"}</span>
+      <span className={styles.response}>{"AVAILABLE COMMANDS:"}</span>
+      <span className={styles.response}>
+        {" - forum login (to log into your account)"}
+      </span>
+      <span className={styles.response}>
+        {" - forum register (to create new account)"}
+      </span>
+    </div>
+  );
+};
+
+export default ConsoleHeader;
