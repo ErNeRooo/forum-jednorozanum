4b18a68573774d70c84f69a2e1ee693f846c16f2
ErNeRooo <erneroooooo@gmail.com>
2024-09-08 12:46:20 +0200

    add firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import HomePage from "./HomePage/HomePage";
 import "./global.sass";
+import { firebaseConfig } from "./firebaseConfig";
+import firebase from "firebase/compat/app";
 export default function Home() {
+  //firebase.initializeApp(firebaseConfig);
+
   return (
     <div>
       <HomePage />
