090d128faecbcf30954102c018289e9709864355
ErNeRooo <erneroooooo@gmail.com>
2024-09-08 10:06:52 +0200

    handling console exception: CommandNotFoundException

--- src/app/components/ConsoleContent/ConsoleContent.tsx
+++ src/app/components/ConsoleContent/ConsoleContent.tsx
@@ -1,26 +1,52 @@
 "use client";
 import styles from "./ConsoleContent.module.sass";
 import ConsolePrompt from "../ConsolePrompt/ConsolePrompt";
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, ReactElement, useState } from "react";
 const ConsoleContent = () => {
-  const [consoleHistory, setConsoleHistory] = useState<string[]>([
-    "Enter Login > HisraEmployee",
-    "Enter Password > ********",
-  ]);
-  const handleOnEnter = (e: BaseSyntheticEvent): void => {
-    setConsoleHistory((prev) => [...prev, e.target.value]);
+  const [consoleHistory, setConsoleHistory] = useState<ReactElement[]>([]);
+  const handleOnEnter = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key !== "Enter") return;
+
+    const target = e.target as HTMLInputElement;
+    const currentCommand = (
+      <span key={consoleHistory.length}>
+        {"PS C:\\forum-jednorozanum> " + target.value}
+      </span>
+    );
+
+    setConsoleHistory((prev) => [...prev, currentCommand]);
+    const cmdProgramString = target.value.split(" ")[0];
+    let underlineString = "";
+    for (let i = 0; i < cmdProgramString.length; i++) {
+      underlineString += "~";
+    }
+
+    if (cmdProgramString !== "forum") {
+      const errorMessage = (
+        <>
+          <span className={styles.errorMessage}>
+            {`${cmdProgramString} : The term \'${cmdProgramString}\' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.`}
+          </span>
+          <span className={styles.errorMessage}>{`At line:1 char:1`}</span>
+          <span className={styles.errorMessage}>{`+ ${target.value}`}</span>
+          <span className={styles.errorMessage}>{`+ ${underlineString}`}</span>
+          <span
+            className={styles.errorMessage}
+          >{` + CategoryInfo : ObjectNotFound: (${cmdProgramString}:String) [], CommandNotFoundException`}</span>
+          <span
+            className={styles.errorMessage}
+          >{` + FullyQualifiedErrorId : CommandNotFoundException`}</span>
+        </>
+      );
+
+      setConsoleHistory((prev) => [...prev, errorMessage]);
+    }
+
+    target.value = "";
   };
   return (
     <div className={styles.ConsoleContent}>
-      {consoleHistory.map((item, index) => {
-        if (item[0] === "P" && item[1] === "S")
-          return <span key={index}>{item}</span>;
-        return (
-          <span className={styles.response} key={index}>
-            {item}
-          </span>
-        );
-      })}
+      {consoleHistory}
       <ConsolePrompt handler={handleOnEnter} />
     </div>
   );
--- src/app/components/ConsolePrompt/ConsolePrompt.tsx
+++ src/app/components/ConsolePrompt/ConsolePrompt.tsx
@@ -1,18 +1,17 @@
 "use client";
-import { BaseSyntheticEvent } from "react";
 import styles from "./ConsolePrompt.module.sass";
 
 const ConsolePrompt = ({ handler }: Props) => {
   return (
     <div className={styles.ConsolePrompt}>
       <span>{"PS C:\\forum-jednorozanum>"}</span>
-      <input type="text" onDragEnterCapture={handler} />
+      <input type="text" onKeyDown={handler} />
     </div>
   );
 };
 
 interface Props {
-  handler: (e: BaseSyntheticEvent) => void;
+  handler: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
 export default ConsolePrompt;
