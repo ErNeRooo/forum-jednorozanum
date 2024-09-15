b5c711a7966abdd398c799e5489cca50945525bd
ErNeRooo <erneroooooo@gmail.com>
2024-09-15 18:25:40 +0200

    add input auto focus

--- src/app/components/ConsoleInput/ConsoleInput.tsx
+++ src/app/components/ConsoleInput/ConsoleInput.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { useEffect, useRef } from "react";
 import styles from "./ConsoleInput.module.sass";
 
 const ConsoleInput = ({ handler, consoleText, type }: Props) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      console.log(typeof inputRef.current);
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <div className={styles.ConsoleInput}>
       <span>{consoleText}</span>
-      <input type={type} onKeyDown={handler} />
+      <input
+        type={type}
+        onKeyDown={handler}
+        ref={inputRef}
+        onBlur={() => inputRef.current && inputRef.current.focus()}
+      />
     </div>
   );
 };
