940fe28f05d91881506456df84a58981efd9c3d3
ErNeRooo <erneroooooo@gmail.com>
2024-09-16 20:19:51 +0200

    create ExplorePage

---
+++ src/app/ExplorePage/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.sass";
+
+const page = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Explore Page</h1>
+    </div>
+  );
+};
+
+export default page;
