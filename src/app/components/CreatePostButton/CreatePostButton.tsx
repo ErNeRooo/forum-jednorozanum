c5c642b4aaef7e2b90b7f9181dde1102abf609a7
ErNeRooo <erneroooooo@gmail.com>
2024-10-05 18:49:57 +0200

    prettier CreatePostButton design

--- src/app/components/CreatePostButton/CreatePostButton.tsx
+++ src/app/components/CreatePostButton/CreatePostButton.tsx
@@ -1,14 +1,34 @@
 "use client";
 import styles from "./CreatePostButton.module.sass";
+import addIcon from "../../../../public/addIcon.svg";
+import Image from "next/image";
+import { CSSProperties, useState } from "react";
 
 const CreatePostButton = ({ setIsFormVisible }: Props) => {
+  const [filter, setFilter] = useState<string>(
+    "invert(47%) sepia(67%) saturate(566%) hue-rotate(73deg) brightness(97%) contrast(83%)"
+  );
   const handleOnClick = (): void => {
     setIsFormVisible(true);
   };
 
+  const iconStyle: CSSProperties = {
+    filter: filter,
+    transform: "scale(2)",
+  };
+
   return (
-    <div className={styles.CreatePostButton} onClick={handleOnClick}>
-      <span>Create Post</span>
+    <div
+      className={styles.CreatePostButton}
+      onClick={handleOnClick}
+      onMouseOver={() => setFilter("brightness(0%)")}
+      onMouseLeave={() =>
+        setFilter(
+          "invert(47%) sepia(67%) saturate(566%) hue-rotate(73deg) brightness(97%) contrast(83%)"
+        )
+      }
+    >
+      <Image src={addIcon} alt="create post" style={iconStyle} />
     </div>
   );
 };
